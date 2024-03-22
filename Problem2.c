20e03ec20a61c4e6773bf6a97f8d03932fea9af4
Yahya Ehab <yahyaehab1856@gmail.com>
2024-03-22 06:13:47 +0200

    solved problem 2

--- Problem2.c
+++ Problem2.c
@@ -1,45 +1,83 @@
 #include <stdio.h>
 #include <string.h>
-#include "mpi.h"
+#include <ctype.h>
+#include <mpi.h>
 
-int main(int argc , char * argv[])
-{
-	int my_rank;		/* rank of process	*/
-	int p;			    /* number of process*/
-	int source;		    /* rank of sender	*/
-	int dest;		    /* rank of receiver	*/
-	MPI_Status status;	/* return status for receive*/
+#define MAX_STRING_LENGTH 100
 
+void encrypt(char* text) {
+    int i;
+    for (i = 0; i < strlen(text); i++) {
+        if (isupper(text[i]))
+            text[i] = 'A' + (text[i] - 'A' + 3) % 26;
+        else if (islower(text[i]))
+            text[i] = 'a' + (text[i] - 'a' + 3) % 26;
+    }
+}
 
-	/* Start up MPI */
-	MPI_Init( &argc , &argv );
+void decrypt(char* text) {
+    int i;
+    for (i = 0; i < strlen(text); i++) {
+        if (isupper(text[i]))
+            text[i] = 'A' + (text[i] - 'A' - 3 + 26) % 26;
+        else if (islower(text[i]))
+            text[i] = 'a' + (text[i] - 'a' - 3 + 26) % 26;
+    }
+}
 
-	/* Find out process rank */
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+int main(int argc, char* argv[]) {
+    int my_rank, p;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-	/* Find out number of process */
-	MPI_Comm_size(MPI_COMM_WORLD, &p);
+    char text[MAX_STRING_LENGTH];
+    int choice;
 
-	int count = 0;
+    if (my_rank == 0) {
+        // Master Process
+        printf("Enter text to encrypt/decrypt: ");
+        fgets(text, MAX_STRING_LENGTH, stdin);
 
-	// ! Slave
-	if( my_rank != 0)
-	{
-		dest = 0;
+        // Get user's choice for encryption/decryption
+        printf("Enter 1 for encryption or 2 for decryption: ");
+        scanf("%d", &choice);
+        getchar(); // Consume newline character
 
-        
-	}
-	// ! Master
-	else
-	{
-        // TODO: Get Mode
-        // TODO: Get encode or decode
-        // TODO: Get filename OR string
-        // TODO: Split string evenly and send to slaves
-        // TODO: Receive results from slaves and print
+        // Broadcast choice to all processes
+        for (int i = 1; i < p; i++) {
+            MPI_Send(&choice, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+        }
 
-	}
+        // Broadcast text to all processes
+        for (int i = 1; i < p; i++) {
+            MPI_Send(text, MAX_STRING_LENGTH, MPI_CHAR, i, 1, MPI_COMM_WORLD);
+        }
+    } else {
+        // Slave Processes
+        MPI_Recv(&choice, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(text, MAX_STRING_LENGTH, MPI_CHAR, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
 
-	MPI_Finalize();
-	return 0;
+    // Master Process
+    if (my_rank == 0) {
+        // Collect results from all slave processes
+        char result[MAX_STRING_LENGTH];
+        for (int i = 1; i < p; i++) {
+            MPI_Recv(result, MAX_STRING_LENGTH, MPI_CHAR, i, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            // Print result from each process
+        }
+        printf("Output: %s\n", result);
+    } else {
+        // Perform encryption or decryption
+        if (choice == 1)
+            encrypt(text);
+        else if (choice == 2)
+            decrypt(text);
+
+        // Send processed text back to Master
+        MPI_Send(text, MAX_STRING_LENGTH, MPI_CHAR, 0, 2, MPI_COMM_WORLD);
+    }
+    MPI_Finalize();
+    return 0;
 }
